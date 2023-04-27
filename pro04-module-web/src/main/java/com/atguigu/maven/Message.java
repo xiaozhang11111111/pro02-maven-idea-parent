878b1af200bd8ad96b2e371e3671082d3f776cad
xiaozhang <1211045482@qq.com>
2023-04-27 17:48:22 +0800

    Initial commit

---
+++ pro04-module-web/src/main/java/com/atguigu/maven/Message.java
@@ -0,0 +1,16 @@
+package com.atguigu.maven;
+
+/**
+ * ClassName: Message
+ * Package: com.atguigu.maven
+ * Description:
+ *
+ * @Author: xiaozhang
+ * @Create 2023/4/21 20:16
+ * @Version: 1.0
+ */
+public class Message {
+     public String getMessage(){
+          return "hello,Message ";
+     }
+}
